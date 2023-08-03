cffb515891a1dc8a896a3f4b83d05f3e5731f2a8
MahithaNama <140938268+MahithaNama@users.noreply.github.com>
2023-08-03 20:46:34 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,10 +3,10 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "magical_instance"{
-ami = "ami-008b85aa3ff5c1b02"
+ami = "ami-0ded8326293d3201b"
 instance_type = "t2.micro"
 key_name = "ansible"
-security_groups = ["launch-wizard-3"]
+security_groups = ["launch-wizard-1"]
 tags={
 Name = "tomcatservers"
 }
