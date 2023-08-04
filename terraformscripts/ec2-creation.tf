016f02c704f45a4c32e5ede53a0d474b36f79f40
MahithaNama <140938268+MahithaNama@users.noreply.github.com>
2023-08-04 18:47:10 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region = "ap-south-1"
 }
 
-resource "aws_instance" "magical_instance"{
+resource "aws_instance" "magic_instance"{
 ami = "ami-008b85aa3ff5c1b02"
 instance_type = "t2.micro"
 key_name = "ansible"
